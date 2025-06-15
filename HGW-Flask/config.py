edfd4f6af4477539217ca88562252688b9784f3c
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-06-15 03:18:53 -0500

    Ajustes para desplegar en vercel y railway

--- HGW-Flask/config.py
+++ HGW-Flask/config.py
@@ -8,5 +8,5 @@ class Config:
     MYSQL_USER = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB = os.getenv("MYSQL_DB")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
     SESSION_COOKIE_SECURE: False
