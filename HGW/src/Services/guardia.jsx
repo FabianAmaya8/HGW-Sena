b3f00793b08bc096bbab83ff7dabd7411ddc4981
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-07-04 10:45:57 -0500

    cambios necesarios

--- HGW/src/Services/guardia.jsx
+++ HGW/src/Services/guardia.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router";
 
 export default function Secure({ children }){
     let navigate = useNavigate();
-    const [hasPermiso, setHasPermiso] = useState(false);
+    const [hasPermiso, setHasPermiso] = useState(true);
     useEffect(()=>{
         if(!hasPermiso){
             navigate("/no-autorizado");
