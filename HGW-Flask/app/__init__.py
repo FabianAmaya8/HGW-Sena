626204d10004bb15023a1465cf8cb771af4535a6
Fabian_Amaya <123851947+FabianAmaya8@users.noreply.github.com>
2025-07-17 15:05:50 -0500

    Cambios para el merge con la rama Fabian

--- HGW-Flask/app/__init__.py
+++ HGW-Flask/app/__init__.py
@@ -30,6 +30,8 @@ def create_app():
     from .controllers.User.Catalogo.producto import stock_bp
     from .controllers.User.Personal.membresia import membresia_bp
     from .controllers.User.Personal.personal import personal_bp
+    from .controllers.User.Carrito.carrito_routes import carrito_bp
+    
     app.register_blueprint(admin_bp)
     app.register_blueprint(mod_bp)
     app.register_blueprint(header_bp)
@@ -39,6 +41,7 @@ def create_app():
     app.register_blueprint(stock_bp)
     app.register_blueprint(membresia_bp)
     app.register_blueprint(personal_bp)
+    app.register_blueprint(carrito_bp)
     with app.app_context():
         from app.models.tablas import tablas, bp_tablas
         tablas.prepare(
